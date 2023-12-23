7f7b527fb7b3a542721b2390bbd9d3daa919c39e
Bruce Cui <53846627+BruceCui1908@users.noreply.github.com>
2023-12-23 15:51:02 +1100

    Delete debug.bat

--- debug.bat
+++
@@ -1,6 +0,0 @@
-docker rm -f streaming
-
-docker build -t streaming:debug -f Dockerfile --progress=plain .
-docker run -it -d -p 8080:8080 -p 1935:1935 -p 10000:10000/udp --name streaming streaming:debug
-
-docker exec -it streaming /bin/bash
